c173567fbbc26b4f3d3792aef1fc504ed6b60007
thinkofmia <thinkofmia@gmail.com>
2021-07-27 23:26:13 +0800

    Added profile pic, repositioned text in goalscene

--- js/scenes/goalScene.js
+++ js/scenes/goalScene.js
@@ -14,6 +14,8 @@ goalScene.init = function(data) {
         this.gameStats = data;
     }
     else this.gameStats = {};
+    this.profilePicPos = [100,100];
+    this.profilePicSize = 0.75;
 }
 
 // load asset files for our game
@@ -30,9 +32,33 @@ goalScene.create = function(){
     let gameW = this.sys.game.config.width;
     let gameH = this.sys.game.config.height;
 
-    let text = this.add.text(gameW/2, gameH/8, 'Goals to level up!', {
-        font: "20px "+this.titleFont,
-        fill: '#ffffff',
+    //Add Profile Pic
+    //Back Frame
+    this.backFramePP = this.physics.add.sprite(this.profilePicPos[0], this.profilePicPos[1], "profile_pic_back");
+    this.backFramePP.setScale(this.profilePicSize);
+    this.backFramePP.body.allowGravity = false;
+    this.backFramePP.depth = 90;
+    //Profile Pic
+    this.profilePicture = this.physics.add.sprite(this.profilePicPos[0], this.profilePicPos[1], this.gameStats.profilePic);
+    this.profilePicture.setScale(this.profilePicSize);
+    this.profilePicture.body.allowGravity = false;
+    this.profilePicture.depth = 90;
+    this.profilePicture.setInteractive();
+    this.profilePicture.on('pointerdown', function(){
+        this.isPlaying = false;
+        this.scene.start('Goal', this.gameStats);
+    }, this);
+    //Front Frame
+    this.frontFramePP = this.physics.add.sprite(this.profilePicPos[0], this.profilePicPos[1], "profile_pic_front");
+    this.frontFramePP.setScale(this.profilePicSize);
+    this.frontFramePP.body.allowGravity = false;
+    this.frontFramePP.depth = 90;
+
+
+    //Add Texts
+    let text = this.add.text(gameW/4, gameH/2, 'Goals to level up!', {
+        font: "25px "+this.titleFont,
+        fill: '#000000',
     });
 
     //Back Button
